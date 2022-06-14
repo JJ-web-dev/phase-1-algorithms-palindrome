7e88fd104327c1349c4e619b245e3b2ad46b096d
Justin James <justinmjames@protonmail.com>
2022-06-14 15:24:29 -0700

    commit tree 09f7893d6fb45908896911ae3b3a109c11bfecac

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
+const { expect } = require("chai");
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const array = word.split('');
+  const reverseArray = array.reverse();
+  const reverseToString = reverseArray.join('')
+
+  if(word == reverseToString) {
+    return true
+  }else {
+    return false
+  }
 }
 
+isPalindrome('mom')
+isPalindrome('rocket')
+
 /* 
-  Add your pseudocode here
+expect a function which takes an argument of a string
+if the argument is a palindrome it will equal true
+if it is not a palindrome it will equal false
+
+return result
 */
 
 /*
-  Add written explanation of your solution here
+  1.create a function that takes an argument of a string
+  2.the function should return true if the string reads the same forward and backward ex. mom and false if it 
+  does not read this way
+  3.the string should only be lowercase letters and no special chars or spaces
+
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  isPalindrome('kayak') = true
+  isPalindrome('rotator') = true 
+  isPalindrome('king') = false 
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
